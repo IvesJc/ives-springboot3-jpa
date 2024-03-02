5eee21dfbcd1432a95e285f04ccacbabbc0b926a
Ives Chiba <ivesjundi@gmail.com>
2024-03-02 18:17:04 -0300

    Nova Classe UserService com aplicação de metodos find e findById

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.educandoweb.course.repositories;
 import com.educandoweb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// PODERIA UTILIZAR O @REPOSITORY, MAS A JPAREPOSITORY JÁ FAZ COM QUE O SPRING IDENTIFIQUE QUE ESSE É UM
+// REPOSITORY
 public interface UserRepository extends JpaRepository<User, Long> {
 
 

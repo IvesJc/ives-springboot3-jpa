74e2def58f0b9ca37d90feab2990ea375ff97f64
Ives Chiba <ivesjundi@gmail.com>
2024-03-04 17:42:02 -0300

    Adding Class Enum OrderStatus

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.educandoweb.course.config;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner {
 
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456789");
 
-        Order o1 = new Order(null, Instant.parse("2024-02-02T19:10:00Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2023-12-14T21:40:00Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2024-07-21T06:30:00Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2024-02-02T19:10:00Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2023-12-14T21:40:00Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2024-07-21T06:30:00Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         // SALVAR OS USUARIOS NO DB
         userRepository.saveAll(Arrays.asList(u1, u2));
